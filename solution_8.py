0a630cf22cc4daf224e460f999958a122b014980
Robert Alef <ralef7@gmail.com>
2015-01-17 18:48:10 -0600

    week1
    Week 1 homework. Correct repository format.

---
+++ solution_8.py
@@ -0,0 +1,11 @@
+#Homework1(Jan13)
+#   A program to convert temperatures from degrees Fahrenheit to degrees Celsius
+# By: Robert Alef
+
+def main():
+    fahrenheit = eval(input("Enter the temperature in degrees Fahrenheit: "))
+    celsius = 5/9 * (fahrenheit - 32)
+    print("The temperature is", celsius, "degrees Celsius")
+
+main()
+ 
